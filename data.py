c394fdcf7e5d92aff4a983e6149611b10162156d
Henry Bigelow <hrbigelow@gmail.com>
2019-02-27 12:54:11 -0500

    progress on checkpointing data

--- data.py
+++ data.py
@@ -26,6 +26,11 @@ from sys import stderr, maxsize
 #    mechanism: in fact, if we adhere to the rule that we only shuffle the whole
 #    catalog each time, then we only need to skip by mod(catalog_size)
 #    But, we do need to initialize the step correctly.
+# Is it worth it to design a checkpointing mechanism that remembers which slice we
+# were in?  That would mean to checkpoint all of the generator states.  So, each
+# instance of a generator would have its own state, and all of these would be
+# checkpointed.  What would that mean?
+
 
 
 
@@ -41,7 +46,22 @@ class MaskedSliceWav(object):
         self.random_seed = np.random.randint(maxsize)
         self.ckpt_position = 0
         
-    # OK
+    # do I need to worry about interrupts here?
+    def save(self, path):
+        import pickle
+        self.rand_state = np.random.get_state()
+        full_state = (self.rand_state, self.epoch, self.file_index,
+                self.slice_indexes)
+        pickle.dump(full_state, path)
+
+    def load(self, path):
+        import pickle
+        (self.rand_state, self.epoch, self.file_index,
+                self.slice_indexes) = pickle.load(path)
+        np.random.set_state(self.rand_state)
+            
+
+
     def init_sample_catalog(self):
         self.sample_catalog = []
         with open(self.sam_file) as sam_fh:
@@ -63,46 +83,41 @@ class MaskedSliceWav(object):
         max_el, _ = max(self.sample_catalog, key=lambda x: x[0])
         return max_el
 
-    # OK
-    def _gen_path(self):
-        '''load a sample file with the format:
-        voice_id /path/to/wav.flac
-        voice_id /path/to/wav.flac
-        ...
-        generate tuples (voice_id, wav_path)
-        '''
-        for s in self.sample_catalog:
-            vid, wav_path = s[0], s[1]
-            #print('Parsing ', wav_path, file=stderr)
-            yield vid, wav_path
+    def _path_gen(self):
+        epoch = self.epoch
+
+        while True:
+            shuffle_catalog = np.random.permutation(self.sample_catalog) 
+            for s, file_index in enumerate(shuffle_catalog):
+                next if epoch == self.epoch and file_index < self.file_index
+                vid, wav_path = s[0], s[1]
+                #print('Parsing ', wav_path, file=stderr)
+                yield epoch, file_index, vid, wav_path
+            epoch += 1
 
 
-    # OK
+    # probably doesn't make sense to do this - rather incorporate librosa.load into
+    # path_gen
     def _wav_gen(self, path_gen):
-        '''consume an iterator that yields [voice_id, wav_path].
-        load the .wav file contents into a vector and return a tuple
-        generate tuples (voice_id, [wav_val, wav_val, ...])
+        '''simple pipe for converting (vid, wav_path) to (vid, [wav,...])
         '''
-        datum_count = self.ckpt_position
         while True:
             try:
-                datum_count += 1
-                vid, wav_path = next(path_gen)
+                epoch, file_index, vid, wav_path = next(path_gen)
                 wav, _ = librosa.load(wav_path, self.sample_rate)
-                #print('loaded wav and mel of size {}'.format(wav.data.nbytes + mel.data.nbytes),
-                #        file=stderr)
-                yield datum_count, int(vid), wav
+                # print('loaded wav of size {}'.format(wav.data.nbytes), file=stderr)
+                yield epoch, file_index, int(vid), wav
             except StopIteration: 
                 break
         return
 
 
-    def _gen_concat_slice_factory(self, wav_gen):
+    def _gen_concat_slice_factory(self, wav_gen, batch_chan):
         '''factory function for creating a new generator function.  The
         returned function encloses wav_gen.
         '''
 
-        def gen_fcn():
+        def slice_gen():
             '''generates a slice from concatenated set of .wav files.
 
             concatenate slices of self.slice_sz where:
@@ -120,7 +135,12 @@ class MaskedSliceWav(object):
                 while len(wav) < self.slice_sz:
                     try:
                         # import pdb; pdb.set_trace()
-                        datum_count, vid, wav_nxt = next(wav_gen) 
+                        epoch, file_index, vid, wav_nxt = next(wav_gen) 
+
+                        slice_gen.epoch = epoch
+                        slice_gen.file_index = file_index
+                        slice_gen.slice_index = 0
+
                         wav_sz = len(wav_nxt)
                         if wav_sz < rf_sz:
                             print(('Warning: skipping length {} wav file (voice id {}).  '
@@ -137,10 +157,15 @@ class MaskedSliceWav(object):
 
                 wav_slice, wav = wav[:self.slice_sz + rf_sz], wav[self.slice_sz:]
                 ids_slice, ids = ids[:self.slice_sz + rf_sz], ids[self.slice_sz:]
-                yield wav_slice, ids_slice
-                return
+                slice_gen.slice_index += 1
+                # Fast-forward if loaded position is 
+                next if slice_gen.slice_index < self.slice_indexes[batch_chan]
 
-        return gen_fcn
+                yield wav_slice, ids_slice
+
+        slice_gen.__dict__ = { 'epoch': 0, 'file_index': 0, 'slice_index': 0 }
+
+        return slice_gen 
 
 
     def _gen_slice_batch(self, path_gen):
@@ -154,19 +179,16 @@ class MaskedSliceWav(object):
         wav_gen = self._wav_gen(path_gen)
 
         # construct batch_sz slice generators, each sharing the same wav_gen
-        gens = [self._gen_concat_slice_factory(wav_gen)() for _ in range(self.batch_sz)]
+        slice_gens = [self._gen_concat_slice_factory(wav_gen)() for _ in range(self.batch_sz)]
 
         while True:
             try:
                 # this is probably expensive
                 # import pdb; pdb.set_trace()
-                batch = [next(g) for g in gens]
-                # cnt represents how many wav file readings have been made
-                # (repeated readings of the same wav file are counted separately)
-                latest_file_read_count = batch[-1][0]
-                wav = np.stack([b[1] for b in batch])
-                ids = np.stack([b[2] for b in batch])
-                yield latest_file_read_count, wav, ids
+                batch = [next(g) for g in slice_gens]
+                wav = np.stack([b[0] for b in batch])
+                ids = np.stack([b[1] for b in batch])
+                yield wav, ids
             except StopIteration:
                 # this will be raised if wav_itr runs out
                 break
@@ -184,7 +206,7 @@ class MaskedSliceWav(object):
         with tf.name_scope('dataset'):
             with tf.name_scope('sample_map'):
                 ds = tf.data.Dataset.from_generator(
-                        self._gen_path,
+                        self._path_gen,
                         (tf.int32, tf.string, tf.string),
                         (zero_d, zero_d, zero_d))
 
