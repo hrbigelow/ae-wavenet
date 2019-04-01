318b3dc7616e3a41c8f4fb2b1c188900a5740beb
Henry Bigelow <hrbigelow@gmail.com>
2019-04-01 12:51:27 -0700

    more rfield

--- rfield.py
+++ rfield.py
@@ -3,14 +3,13 @@
 
 
 class FieldOffset(object):
-    '''calculate left and right receptive field offsets for various situations.
-    self.left_ind and self.right_ind are in tensor index coordinate offsets.
-    self.left and self.right are in external coordinates, related by
-    field_spacing.
-    
-    field_spacing is some positive integer, which represents how far apart in
-    the external coordinate system are a consecutive pair of output tensor
-    elements.
+    '''
+    Use this class as a member in individual Convolution or Transpose
+    convolution modules.  Allows convenient back-calculation of needed input
+    size for a desired output size.  Also, allows calculating the left and
+    right offsets of the output from the input across the whole chain of
+    transformations.
+
     '''
     def __init__(self, padding=(0, 0), wing_sizes=None, filter_sz=None,
             stride=1, is_downsample=True, parent=None):
@@ -21,10 +20,12 @@ class FieldOffset(object):
         self.is_downsample = is_downsample 
         self.left_pad = padding[0]
         self.right_pad = padding[1]
+
+        # stride_ratio is ratio of output spacing to input spacing
         if self.is_downsample:
-            self.stride_ratio = 1.0 / self.stride
-        else:
             self.stride_ratio = self.stride
+        else:
+            self.stride_ratio = 1.0 / self.stride
 
         if isinstance(wing_sizes, tuple):
             self.left_wing_sz = wing_sizes[0]
@@ -33,6 +34,9 @@ class FieldOffset(object):
             total_wing_sz = filter_sz - 1
             self.left_wing_sz = total_wing_sz // 2
             self.right_wing_sz = total_wing_sz - self.left_wing_sz
+        else:
+            raise RuntimeError('Must be called with either '
+            'wing_sizes tuple or filter_sz (integer)')
 
     def __repr__(self):
         return 'left_wing_sz: {}, right_wing_sz: {}, stride: {}, is_downsample: {}'.format(
@@ -58,42 +62,58 @@ class FieldOffset(object):
         if self.parent is None:
             return this_input_size 
         else:
-            return self.input_size(this_input_size)
+            return self.parent.input_size(this_input_size)
 
-    def _left(self):
+    def _local_bounds(self):
+        '''
+        '''
+        padded_stride = 1 if self.is_downsample else self.stride_ratio
+        l_ind = self.left_wing_sz - self.left_pad
+        r_ind = self.right_wing_sz - self.right_pad
+        l_off = l_ind * padded_stride
+        r_off = r_ind * padded_stride
+        return l_off, r_off
+
+    def _bounds(self):
         if self.parent is None:
-            pos_cumul, sr_cumul = 0, 1 
+            l_in_pos, r_in_pos, in_stride = 0, 0, 1 
         else:
-            pos_cumul, sr_cumul = self._left()
+            l_in_pos, r_in_pos, in_stride = self.parent._bounds()
 
-        left_ind = self.left_wing_sz - self.left_pad
-        pos = pos_cumul + left_ind * sr_cumul
-        return (pos, sr_cumul * self.stride_ratio)
+        l_off, r_off = self._local_bounds()
+
+        # Accumulate offsets
+        l_out_pos = l_in_pos + l_off * in_stride
+        r_out_pos = r_in_pos + r_off * in_stride
+        out_stride = self.stride_ratio * in_stride
+
+        return l_out_pos, r_out_pos, out_stride 
         
-    def left(self):
+    def bounds(self):
         '''Considering the full chain of transformations, calculate the number
-        of positions the start of the input is to the left of the start of the
-        output.'''
-        pos, stride_ratio = self._left()
-        return pos
+        of positions the output boundaries are inset from the input boundaries.
+        '''
+        l_pos, r_pos, stride = self._bounds()
+        return l_pos, r_pos 
 
-    def _right(self):
+    def _min_stride(self, pre_stride, min_stride):
+        '''Traverse the chain of transformations, recording the minimal stride
+        seen'''
+        cur_stride = pre_stride / self.stride_ratio
+        min_stride = min(cur_stride, min_stride)
+
         if self.parent is None:
-            return (0, self.stride_ratio)
+            return min_stride
         else:
-            pos_cumul, sr_cumul = self._right()
-            right_ind = self.right_wing_sz - self.right_pad
-            pos = pos_cumul - right_ind * sr_cumul
-            return (pos, sr_cumul * self.stride_ratio)
+            return self.parent._min_stride(cur_stride, min_stride)
 
-    def right(self):
-        '''Considering the full chain of transformations, calculate the number
-        of positions the end of the input is to the right of the end of the
-        output'''
-        pos, stride_ratio = self._right()
-        return pos
 
-    
+    def print(self, output_size, lpad_rpad_inpad_data_sym='<>-*'):
+        '''print a symbolic stack of units with the given output_size'''
+        lpad, rpad, ipad, data = list(lpad_rpad_inpad_data_sym)
+        span = self.input_size(output_size)
+        min_stride = self._min_stride(1, 1)
+
 
 
 
