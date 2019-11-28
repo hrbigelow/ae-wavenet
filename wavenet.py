4128fcfc441a7bd5147e704f00e33201c3a49a11
Henry Bigelow <hrbigelow@gmail.com>
2019-11-27 21:40:59 -0800

    remove proj_signal + proj_gate

--- wavenet.py
+++ wavenet.py
@@ -90,8 +90,10 @@ class GatedResidualCondConv(nn.Module):
         #cond_lead = self.cond_lead()
         #skip_lead = self.skip_lead()
 
-        filt = self.conv_signal(x) + self.proj_signal(cond[:,:,self.cond_lead:])
-        gate = self.conv_gate(x) + self.proj_gate(cond[:,:,self.cond_lead:])
+        # filt = self.conv_signal(x) + self.proj_signal(cond[:,:,self.cond_lead:])
+        filt = self.conv_signal(x)
+        # gate = self.conv_gate(x) + self.proj_gate(cond[:,:,self.cond_lead:])
+        gate = self.conv_gate(x)
         z = torch.tanh(filt) * torch.sigmoid(gate)
         sig = self.dil_res(z)
         skp = self.dil_skp(z[:,:,self.skip_lead:])
