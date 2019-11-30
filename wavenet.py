1c19037e2a385b5852103da1c13f86294026de0d
Henry Bigelow <hrbigelow@gmail.com>
2019-11-30 10:59:02 -0800

    no backward, no lc_dense_trim take

--- wavenet.py
+++ wavenet.py
@@ -260,8 +260,9 @@ class WaveNet(nn.Module):
         # W2 = lcond_slice.size()[1] 
 
         D2 = lc_dense.size()[1]
-        lc_dense_trim = torch.take(lc_dense,
-                lcond_slice.unsqueeze(1).expand(-1, D2, -1))
+        # lc_dense_trim = torch.take(lc_dense,
+        #         lcond_slice.unsqueeze(1).expand(-1, D2, -1))
+        lc_dense_trim = lc_dense[:,:,:2146]
 
         cond = self.cond(lc_dense_trim, speaker_inds)
         # "The conditioning signal was passed separately into each layer" - p 5 pp 1.
