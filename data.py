3463809a34994492e29da0a2d329e1177b1035cc
Henry Bigelow <hrbigelow@gmail.com>
2019-12-05 18:05:50 -0800

    remove print message

--- data.py
+++ data.py
@@ -115,10 +115,9 @@ class VirtualBatch(object):
         for b, wi in enumerate(picks):
             s, voice_ind = ds.in_start[wi]
             wav_enc_input = ds.snd_data[s:s + ds.enc_in_len]
-            print('wav_enc_input.shape: {}, s: {}, ds.snd_data.shape: {}'.format(
-                wav_enc_input.shape, s, ds.snd_data.shape), file=stderr)
-            stderr.flush()
-
+            # print('wav_enc_input.shape: {}, s: {}, ds.snd_data.shape: {}'.format(
+            #     wav_enc_input.shape, s, ds.snd_data.shape), file=stderr)
+            # stderr.flush()
 
             self.wav_dec_input[b,...] = wav_enc_input[trim[0]:trim[1]]
             self.mel_enc_input[b,...] = ds.mfcc_proc.func(wav_enc_input)
