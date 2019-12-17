5ff3f5d1d991fb524c1cba61fd4c16b96e9761da
Henry Bigelow <hrbigelow@gmail.com>
2019-12-17 15:11:55 -0800

    add mu sigma stats

--- vae_bn.py
+++ vae_bn.py
@@ -115,7 +115,12 @@ class SGVBLoss(nn.Module):
             log_pred_loss + self.anneal_weight 
             * torch.clamp(kl_div_loss, min=self.free_nats))
 
-        self.metrics = { 'kl_div_loss': kl_div_loss, 'log_pred_loss': log_pred_loss }
+        self.metrics = {
+                'kl_div_loss': kl_div_loss,
+                'log_pred_loss': log_pred_loss,
+                'mu_abs_max': mu.abs().max(),
+                's_sq_abs_max': sigma_sq.abs().max()
+                }
 
         return total_loss 
 
