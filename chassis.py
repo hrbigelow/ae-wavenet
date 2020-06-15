5db381ce800b9aea0521acc4f22f47b761a5ca20
Henry Bigelow <hrbigelow@gmail.com>
2020-06-15 11:28:56 -0700

    try add_histogram with cpu tensor

--- chassis.py
+++ chassis.py
@@ -179,12 +179,12 @@ class Chassis(object):
                 print(f'after uw_ratio calc', file=stderr)
                 stderr.flush()
 
-                self.writer.add_scalar('test',
-                        t.tensor([4.6], device=t.device('cpu')),
-                        ss.optim_step)
-                # for name, par in ss.model.named_parameters():
-                #     if self.writer is not None:
-                #         self.writer.add_histogram(name, par.data, ss.optim_step)
+                # self.writer.add_scalar('test',
+                #         t.tensor([4.6], device=t.device('cpu')),
+                #         ss.optim_step)
+                for name, par in ss.model.named_parameters():
+                    if self.writer is not None:
+                        self.writer.add_histogram(name, par.data.cpu(), ss.optim_step)
 
                 if self.is_tpu:
                     xm.rendezvous('add_histogram')
